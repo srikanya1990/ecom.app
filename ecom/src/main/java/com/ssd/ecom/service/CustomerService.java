47eeb3bc55d2075dabbc2b125fc546a338ce9245
satyavathi23 <satyakad60@gmail.com>
2023-07-22 16:33:58 +0530

    changes for retrieve by id developS002

--- ecom/src/main/java/com/ssd/ecom/service/CustomerService.java
+++ ecom/src/main/java/com/ssd/ecom/service/CustomerService.java
@@ -1,6 +1,7 @@
 package com.ssd.ecom.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,9 @@ public class CustomerService {
 		
 		return customerRepository.findAll();
 	}
-
+     
+     public Optional<Customer> getauthorbyid(Long id){
+		
+		return customerRepository.findById(id);
+	}
 }
