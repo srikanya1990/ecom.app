d8d42d8ebe51f5d2db73f0748cb2b7d57b290867
SRI KANYA <SRI KANYA@Srikanya>
2023-07-22 14:16:03 +0530

    change for S001

--- ecom/src/main/java/com/ssd/ecom/contoller/CustomerRestController.java
+++ ecom/src/main/java/com/ssd/ecom/contoller/CustomerRestController.java
@@ -25,6 +25,11 @@ public class CustomerRestController {
 	public String getWelcomeMsg() {
 		return "Welocme SSD Ecom App";
 	}
+
+	@GetMapping("/otp")
+	public String getOtp() {
+		return "34562";
+	}
 	@PostMapping("/create")
     public ResponseEntity<Customer> createCustomer(@RequestBody  Customer customer){
     	Customer cust = customerService.createCustomer(customer);
